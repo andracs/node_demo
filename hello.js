9436f1bde289f674a211b12ca44894ef9e271f2e
Andras Acs <acsandras@gmail.com>
2018-02-27 10:08:47 +0100

    Initial commit

---
+++ hello.js
@@ -0,0 +1,2 @@
+let message = "Hello Datamatikere";
+console.log(message);
---
+++ readfile.js
@@ -0,0 +1,6 @@
+const {readFile} = require("fs");
+readFile("file.txt", (error, buffer) => {
+    if (error) throw error;
+console.log("The file contained", buffer.length, "bytes.",
+    "The first byte is:", buffer[0]);
+});
